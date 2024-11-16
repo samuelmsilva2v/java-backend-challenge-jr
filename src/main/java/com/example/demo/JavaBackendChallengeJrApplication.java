988f5b301631bdb66f918adfb5591ac4eda1193c
samuelmsilva2v <samuelmsilva@outlook.com.br>
2024-11-16 17:06:56 -0300

    first commit

---
+++ src/main/java/com/example/demo/JavaBackendChallengeJrApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBackendChallengeJrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaBackendChallengeJrApplication.class, args);
+	}
+
+}
